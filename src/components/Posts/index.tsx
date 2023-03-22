55bcd4e47daea6ace96f5c96f5b3073a6ee7def6
Pedro Furlan <pedrofurlan6@gmail.com>
2023-03-22 11:01:08 -0300

    Create getPosts

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,8 @@
+import { useEffect, useState } from "react";
+import api from "../../lib/axios";
+
+export function Posts() {
+  
+
+  return;
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,32 @@
+import { useEffect, useState } from "react";
+import api from "../../lib/axios";
+
 export function Home() {
+  interface PostProps {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
+  }
+
+  const [posts, setPosts] = useState<PostProps[]>([]);
+  async function getPosts() {
+    await api.get("/posts").then((response) => setPosts(response.data));
+  }
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
-    <div>Olá</div>
+    <ul>
+      {posts && (
+        posts.map((post) =>
+          <li key={post.id}>
+            <b>{post.body}</b>
+          </li>
+        )
+      )}
+    </ul>
   )
 }
