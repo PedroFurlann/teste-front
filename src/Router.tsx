fb15cfcbca5099861b23e4e3f25d79296c3b673e
Pedro Furlan <pedrofurlan6@gmail.com>
2023-03-24 17:01:29 -0300

    Update Router.tsx

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Feed } from "./pages/Feed";
 import { Home } from "./pages/Home";
+import { Users } from "./pages/Users";
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/feed" element={<Feed />} />
+      <Route path="/user/:userId" element={<Users />} />
     </Routes>
   );
 }
